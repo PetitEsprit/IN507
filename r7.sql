65fb73bb670f69bd5ed884bee7c41250b1a2e806
Utilisateur <user@debian.arcahe.ovh>
2019-10-09 16:50:47 +0200

    holli

--- r7.sql
+++ r7.sql
@@ -1,1 +1,5 @@
-
+SELECT malade.numero, malade.nom, malade.prenom, COUNT(soigne.no_docteur) AS nb_soignants ,COUNT(DISTINCT docteur.specialite) AS nb_specialites FROM `docteur`, `soigne`, `malade`
+WHERE docteur.numero = soigne.no_docteur AND soigne.no_malade = malade.numero
+GROUP BY malade.numero
+HAVING nb_soignants > 3
+ORDER BY malade.nom ASC
