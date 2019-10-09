3c59a26a3e45bcf1a4811f0fd03b2513afaa422b
Utilisateur <user@debian.arcahe.ovh>
2019-10-09 15:48:21 +0200

    enfingit add *!

--- r4.sql
+++ r4.sql
@@ -1,1 +1,5 @@
-
+SELECT chambre.no_chambre, hospitalisation.lit, service.nom, malade.prenom, malade.nom, malade.mutuelle FROM `hospitalisation`, `chambre`, `malade`, `service` 
+WHERE hospitalisation.no_malade = malade.numero AND chambre.code_service = service.code AND hospitalisation.code_service = chambre.code_service
+AND chambre.no_chambre = hospitalisation.no_chambre
+AND service.batiment = "B"
+AND malade.mutuelle LIKE "MN%"
