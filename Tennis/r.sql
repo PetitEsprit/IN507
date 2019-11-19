1ac5fdd7d30383a036067dfe7de592ee00c91216
Mathis Dankou <PetitEsprit@users.noreply.github.com>
2019-11-19 18:03:38 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah

--- Tennis/r.sql
+++ Tennis/r.sql
@@ -17,5 +17,24 @@ SELECT DISTINCT nomgagnant FROM `Rencontre`
 UNION
 SELECT DISTINCT nomperdant FROM `Rencontre`
 #
-
-
+SELECT DISTINCT nomjoueur FROM `Gain`
+WHERE lieutournoi='Winbledon'
+#
+SELECT nomgagnant FROM `Rencontre` WHERE nomgagnant NOT IN (SELECT nomperdant FROM `Rencontre`)
+#
+SELECT nomjoueur FROM `Gain`
+WHERE nomjoueur NOT IN
+(SELECT nomjoueur FROM `Gain` WHERE prime < 1000000)
+#
+SELECT COUNT(lieutournoi) AS nbmatchs FROM `Rencontre`
+WHERE lieutournoi = 'US Open'
+#
+SELECT SUM(prime) AS GrossMoulahNadalPosay FROM `Gain` WHERE nomjoueur = 'Nadal'
+#
+SELECT nomjoueur, SUM(prime) AS gaintotal FROM `Gain` GROUP BY nomjoueur HAVING gaintotal >= 2000000
+#
+SELECT Sprime.annee, Sprime.lieutournoi FROM (SELECT lieutournoi, annee, SUM(prime) AS sgain FROM `Gain`
+GROUP BY lieutournoi, annee) AS Sprime, (SELECT lieutournoi, annee, SUM(montant) AS smontant FROM `Sponsor`
+GROUP BY lieutournoi, annee) AS Ssponsor
+WHERE Ssponsor.lieutournoi = Sprime.lieutournoi AND Ssponsor.annee = Sprime.annee
+AND Ssponsor.smontant > Sprime.sgain
